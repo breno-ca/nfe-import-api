4d41987aa8cf66516299879f471eeac5b65ba5b5
Breno AA <breno.de.avila@gmail.com>
2023-10-26 23:26:04 -0300

    criação do servidor e rotas

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -8,11 +8,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Struct com as configurações do servidor
 type Server struct {
 	SRV_PORT string
 	SERVER   *gin.Engine
 }
 
+// Construtor do Server
 func NewServer(conf *config.Config) Server {
 	return Server{
 		SRV_PORT: conf.SRV_PORT,
@@ -20,6 +22,7 @@ func NewServer(conf *config.Config) Server {
 	}
 }
 
+// Inicia o router Gin-gonic
 func Run(router *gin.Engine, server Server, service service.UserServiceInterface) {
 	log.Print("Server is running at port: ", server.SRV_PORT)
 	log.Fatal(router.Run(":" + server.SRV_PORT))

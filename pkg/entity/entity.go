8f8cbfdd819696ceaeeca6628e8624840bdc5152
Breno AA <breno.de.avila@gmail.com>
2023-10-26 23:22:31 -0300

    entidades para importação da nfe

--- pkg/entity/entity.go
+++ pkg/entity/entity.go
@@ -5,24 +5,49 @@ type User struct {
 	Senha string `json:"password"`
 }
 
+type NfeProc struct {
+	NFe NFe `xml:"NFe"`
+}
+
 type NFe struct {
-	Emit       Emit   `xml:"NFe>infNFe>emit"`
-	Dest       Dest   `xml:"NFe>infNFe>dest"`
-	Prod       []Prod `xml:"NFe>infNFe>det"`
-	VAdicional float64
-	VMargem    float64
+	InfNFe InfNFe `xml:"infNFe"`
+}
+
+type InfNFe struct {
+	Emit Emit `xml:"emit"`
+	Dest Dest `xml:"dest"`
+	Det  Det  `xml:"det"`
 }
 
 type Emit struct {
-	CNPJ  string `xml:"CNPJ"`
-	XNome string `xml:"xNome"`
+	CNPJ string `xml:"CNPJ"`
+	// XNome     string    `xml:"xNome"`
+	// XFant     string    `xml:"xFant"`
+	// EnderEmit EnderEmit `xml:"enderEmit"`
+	// IE        string    `xml:"IE"`
+	// CRT       string    `xml:"CRT"`
+}
+
+type EnderEmit struct {
+	XLgr    string `xml:"xLgr"`
+	Nro     string `xml:"nro"`
+	XCpl    string `xml:"xCpl"`
+	XBairro string `xml:"xBairro"`
+	CMun    string `xml:"cMun"`
+	XMun    string `xml:"xMun"`
+	UF      string `xml:"UF"`
+	CEP     string `xml:"CEP"`
+	CPais   string `xml:"cPais"`
+	XPais   string `xml:"xPais"`
+	Fone    string `xml:"fone"`
 }
 
 type Dest struct {
 	CNPJ      string    `xml:"CNPJ"`
 	XNome     string    `xml:"xNome"`
-	Email     string    `xml:"email"`
 	EnderDest EnderDest `xml:"enderDest"`
+	// IndIEDest string    `xml:"indIEDest"`
+	// IE        string    `xml:"IE"`
 }
 
 type EnderDest struct {
@@ -31,8 +56,16 @@ type EnderDest struct {
 	XCpl    string `xml:"xCpl"`
 	XBairro string `xml:"xBairro"`
 	CMun    string `xml:"cMun"`
-	CEP     string `xml:"CEP"`
-	Fone    string `xml:"fone"`
+	// XMun    string `xml:"xMun"`
+	// UF      string `xml:"UF"`
+	CEP string `xml:"CEP"`
+	// CPais   string `xml:"cPais"`
+	// XPais   string `xml:"xPais"`
+	// Fone    string `xml:"fone"`
+}
+
+type Det struct {
+	Prod []*Prod `xml:"prod"`
 }
 
 type Prod struct {
@@ -42,6 +75,24 @@ type Prod struct {
 	UCom   string  `xml:"uCom"`
 	QCom   float64 `xml:"qCom"`
 	VUnCom float64 `xml:"vUnCom"`
-	VCusto float64
-	VPreco float64
+	// QTrib   float64 `xml:"qTrib"`
+	// VUnTrib float64 `xml:"vUnTrib"`
+	VFrete float64 `xml:"vFrete"`
+	VSeg   float64 `xml:"vSeg"`
+	VDesc  float64 `xml:"vDesc"`
+	VOutro float64 `xml:"vOutro"`
+	VProd  float64 `xml:"vProd"`
+}
+
+type Total struct {
+	ICMSTot ICMSTot `json:"ICMSTot"`
+}
+
+type ICMSTot struct {
+	VProd      float64 `json:"vProd"`
+	VDesc      float64 `json:"vDesc"`
+	VFrete     float64 `json:"vFrete"`
+	VOutro     float64 `json:"vOutro"`
+	VAdicional float64 `json:"vAdicional"`
+	VMargem    float64 `json:"vMargem"`
 }
